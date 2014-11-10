293d61002c1ddd71832f957eb210cb0c79335f9e
AdrianCanoGarcia <adrianmota@gmail.com>
2014-11-10 20:00:02 +0100

    express implemented

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,23 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-var user={};
-
+var users = {};
+var numUser = 0;
 /* ROUTES */
 router.post('/', createUser);
 router.put('/:userId/set/:name', setUser);
 router.get('/:userId', getUser);
 router.delete('/:userId', delUser);
 
-function createScore(req, res) {
-	var score = {
-		_id: String(numScores),
-		home: 0,
-		guest: 0
-	};
-	
-	scores[score._id] = score;
-	numScores++;
-  
- res.json(score);
-}; 
+function createUser(req, res) {
+    var user = {
+        _id: String(numUser),
+        name: " "
+    };
+    user[user._id] = user;
+    numUser++;
+
+    res.json(user);
+}
+function setUser(req, res) {
+    var user = {
+        _id: String(numUser),
+        name: req.params.name,
+    };
+    user[req.params._id] = user;
+    numUser++;
+
+    res.json(user);
+}
+
+function delUser(req, res) {
+    delete users[req.params.userId];
+
+    res.send('scored ' + req.params.userId + ' removed.');
+}
+function getUser(req, res) {
+    res.json(req.User);
+}
