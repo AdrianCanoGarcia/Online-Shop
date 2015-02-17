b1e15140bbae5dcba6150e81c86ae6cf97835561
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2015-02-17 14:21:07 +0100

    API ebay & pagination

--- server/manager/ebay.js
+++ server/manager/ebay.js
@@ -1,13 +1,12 @@
 var request = require('request');
-          
-function getApi(item, callback){
-    request('http://open.api.ebay.com/shopping?version=713&'+'appid=Ikaruscfa-4fc4-4be7-a6b0-4e54505e4f8&'+'callname=FindItems&'+'QueryKeywords='+item+'&ResponseEncodingType=JSON&'+'MaxEntries=10', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        callback(body);
-      }
-})   
 
+function getApi(item, callback) {
+  request('http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=Ikaruscfa-4fc4-4be7-a6b0-4e54505e4f8&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords='+item+'&paginationInput.entriesPerPage=50', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(body);
+    }
+  })
 }
 module.exports = {
-    getApi:getApi
+  getApi: getApi
 };
